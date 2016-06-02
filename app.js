49516ef0cb8363598a11768be8efcc3063a5a5f3
Jake Priddy <priddyjacob84@gmail.com>
2016-06-02 19:35:31 -0400

    6/2 1935

---
+++ app.js
@@ -0,0 +1,14 @@
+var fizzBuzz= function(){//count from 1 to 100
+for (var i = 0; i <=100; i++ ){
+//if divisible by 3 log fizz
+    } if ( i / 3 && i%=0) {
+//if divisible by 5 log buzz
+console.log("fizz");
+    }else if (i/5 && i%=0){
+//if divisible by 3 && 5 log fizzBuzz
+console.log("buzz");
+    }else if (i=((i/3) && (i/5) &&(i%=0) {
+                 console.log("Fizz Buzz");
+                 };
+              };
+              conosle.log(fizzbuzz)
