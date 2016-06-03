7bab1295972b721f984d9ba2670179c26a99b8e5
Jake Priddy <priddyjacob84@gmail.com>
2016-06-03 04:58:25 -0400

    6/3 0458

--- app.js
+++ app.js
@@ -4,27 +4,27 @@ var fizzBuzz = [];
 //Done---create a for loop
 for (var i = 0; i <= 100; i++) {
 
-//Done---if the number when divided by 3 do something
-if (i % 3 === 0) {
-     fizzBuzz.push("Fizz"); 
-//  replace it with fizz  
+    //Done---if the number when divided by 3 do something
+    if ((i % 3) === 0) {
+        fizzBuzz.push("Fizz"); 
+        //  replace it with fizz  
 
-    //has a modulo equal to 0 log fizz
-//if divisible by 5 log buzz
-} else if ((i % 5) === 0) {
-////if the number when divided by 5 has a 
+        //has a modulo equal to 0 log fizz
+        //if divisible by 5 log buzz
+    } else if ((i % 5) === 0) {
+        ////if the number when divided by 5 has a 
         //splice buzz
-     fizzBuzz.push("Buzz"); 
-//        //modulo equal to zero log buzz
-//console.log("buzz");
- } else if (((i % 3)===0) && ((i % 5) === 0)) {
-//                 //if divisible by 3 && 5 && has a modulo equal to zero for both log fizzBuzz
+        fizzBuzz.push("Buzz"); 
+        //        //modulo equal to zero log buzz
+        //console.log("buzz");
+    } else if (((i % 3) === 0) && ((i % 5) === 0)) {
+        //                 //if divisible by 3 && 5 && has a modulo equal to zero for both log fizzBuzz
         //slice fizzbuzz
-     fizzBuzz.push("Fizz Buzz")
-}
+        fizzBuzz.push("Fizz Buzz")
+    }
     //Done---log the for loop into an array
     fizzBuzz.push(i);
 };
-             
-              
-              //run the fizzbuzz variable
+
+
+//run the fizzbuzz variable
