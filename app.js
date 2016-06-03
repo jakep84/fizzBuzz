648fdfdef67b844e1466ad44675b600f4c920ca0
Jake Priddy <priddyjacob84@gmail.com>
2016-06-03 04:55:24 -0400

    6/3 0455

--- app.js
+++ app.js
@@ -2,25 +2,28 @@
 //Done---create an empty array 
 var fizzBuzz = [];
 //Done---create a for loop
-for (var i = 0; i <=100; i++ ) {
-//Done---log the for loop into an array
-fizzBuzz.push(i);
-//if the number when divided by 3's modulos answer equals zero splice fizz  
+for (var i = 0; i <= 100; i++) {
+
+//Done---if the number when divided by 3 do something
 if (i % 3 === 0) {
-    alert("yahtzee!");
-//fizzBuzz.replace((i % 3 == 0), "fizz" );
+     fizzBuzz.push("Fizz"); 
+//  replace it with fizz  
+
     //has a modulo equal to 0 log fizz
 //if divisible by 5 log buzz
-//    } else if ((i/5) % == 0) {
+} else if ((i % 5) === 0) {
 ////if the number when divided by 5 has a 
         //splice buzz
+     fizzBuzz.push("Buzz"); 
 //        //modulo equal to zero log buzz
 //console.log("buzz");
-//    } else if ((i/3) && (i/5) % = 0) {
+ } else if (((i % 3)===0) && ((i % 5) === 0)) {
 //                 //if divisible by 3 && 5 && has a modulo equal to zero for both log fizzBuzz
         //slice fizzbuzz
-//              return i;
-    }
+     fizzBuzz.push("Fizz Buzz")
+}
+    //Done---log the for loop into an array
+    fizzBuzz.push(i);
 };
              
               
