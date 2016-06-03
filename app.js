48829e6d5f4b9d8d7c92afd727fb152a38633f05
Jake Priddy <priddyjacob84@gmail.com>
2016-06-02 23:56:11 -0400

    6/2 2355

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-var fizzBuzz= function(){//count from 1 to 100
-for (var i = 0; i <=100; i++ ){
-//if divisible by 3 log fizz
-    } if ( i / 3 && i%=0) {
+//log 1 to 100
+for (var i = 0; i <=100; i++ ) {
+    } if ( (i / 3) % = 0) {
     //if the number when divided by 3
     //has a modulo equal to 0 log fizz
 //if divisible by 5 log buzz
 console.log("fizz");
-    }else if (i/5 && i%=0){
+    } else if ((i/5) % = 0) {
 //if the number when divided by 5 has a 
         //modulo equal to zero log buzz
 console.log("buzz");
-    }else if (i=((i/3) && (i/5) &&(i%=0) {
+    } else if ((i/3) && (i/5) % = 0) {
                  //if divisible by 3 && 5 && has a modulo equal to zero for both log fizzBuzz
-                 console.log("Fizz Buzz");
-                 };
-              };
+              return i;
+    }
+             
               
               //run the fizzbuzz variable
-              conosle.log(fizzbuzz)
+console.log("fizzbuzz");
