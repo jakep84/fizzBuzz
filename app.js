ccb749fa2af63384ff1826adfd7577f81a538439
Jake Priddy <priddyjacob84@gmail.com>
2016-06-03 02:58:01 -0400

    6/3 0257

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 //log 1 to 100
 for (var i = 0; i <=100; i++ ) {
-    } if ( (i / 3) % = 0) {
+    } if ( (i / 3) % == 0) {
     //if the number when divided by 3
+        //splice fizz
     //has a modulo equal to 0 log fizz
 //if divisible by 5 log buzz
-console.log("fizz");
-    } else if ((i/5) % = 0) {
-//if the number when divided by 5 has a 
-        //modulo equal to zero log buzz
-console.log("buzz");
-    } else if ((i/3) && (i/5) % = 0) {
-                 //if divisible by 3 && 5 && has a modulo equal to zero for both log fizzBuzz
-              return i;
+console.log(i);
+//    } else if ((i/5) % == 0) {
+////if the number when divided by 5 has a 
+        //splice buzz
+//        //modulo equal to zero log buzz
+//console.log("buzz");
+//    } else if ((i/3) && (i/5) % = 0) {
+//                 //if divisible by 3 && 5 && has a modulo equal to zero for both log fizzBuzz
+        //slice fizzbuzz
+//              return i;
     }
              
               
               //run the fizzbuzz variable
-console.log("fizzbuzz");
